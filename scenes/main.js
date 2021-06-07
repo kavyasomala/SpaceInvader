fc0b169d47b004e0102d04e419e26e5fccbb000a
Kavya Somala <somala.kavya123@gmail.com>
2021-06-07 18:25:25 +0000

    Finished the tutorial for building this game, made a list of improvements to work on later.

--- scenes/main.js
+++ scenes/main.js
@@ -1,9 +1,9 @@
-
 const MOVE_SPEED = 200
-const TIME_LEFT = 200 //change later
+const TIME_LEFT = 30
 const INVADER_SPEED = 100
 let CURRENT_SPEED = INVADER_SPEED
 const LEVEL_DOWN = 100
+const BULLET_SPEED = 400
 
 
 layer(['obj', 'ui'], 'obj')
@@ -51,11 +51,35 @@ keyDown('right', () => {
   player.move(MOVE_SPEED, 0)
 })
 
+function spawnBullet(p) {
+  add([rect(6, 18), pos(p), origin('center'), color(0.5, 0.5, 1), 'bullet'])
+}
+
+keyPress('space', () => {
+  spawnBullet(player.pos.add(0, -25))
+})
+
+
+action('bullet', (b) => {
+  b.move(0, -BULLET_SPEED)
+  if (b.pos.y < 0) {
+    destroy(b)
+  }
+})
+
+collides('bullet', 'space-invader', (b, s) => {
+  camShake(4)
+  destroy(b)
+  destroy(s)
+  score.value++
+  score.text = score.value
+})
+
 const score = add([
   text('0'),
-  pos(50, 50),
+  pos(58, 55),
   layer('ui'),
-  scale(3),
+  scale(2),
   {
     value: 0,
   }
@@ -63,8 +87,8 @@ const score = add([
 
 const timer = add([
   text('0'),
-  pos(90, 50),
-  scale(2),
+  pos(58, 22),
+  scale(2.5),
   layer('ui'),
   {
     time: TIME_LEFT,
@@ -75,7 +99,7 @@ timer.action(() => {
   timer.time -= dt()
   timer.text = timer.time.toFixed(2)
   if (timer.time <= 0) {
-    go('lose', score.value)
+    go('lose', { score: score.value })
   }
 })
 
@@ -98,11 +122,23 @@ collides('space-invader', 'left-wall', () => {
 })
 
 player.overlaps('space-invader', () =>
-  go('lose', {score: score.value})
+  go('lose', { score: score.value })
 )
 
 action('space-invader', (s) => {
-  if(s.pos.y >= (height() - height()/4)){
-      go('lose', {score: score.value})
+  if (s.pos.y >= (height() - height() / 4)) {
+    go('lose', { score: score.value })
   }
 })
+
+///////////////////////////////
+/*
+More things to add:
+ - Add win scene
+ - Add new levels
+ - Be able to choose easy medium or hard
+ - Make game responsive
+  - position space ship so it will work on any browser size
+  - account for browser size vs game size for space invaders lose condition
+ - prevent the "space ship" from being able to go offscreen
+*/
